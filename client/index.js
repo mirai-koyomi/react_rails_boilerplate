363a239dc60d07be0bb01a00c20202f231052c20
藤本　聖也 <seiya_fujimoto@waku-2.com>
2018-06-15 18:17:42 +0900

    chore(eslinter): eslinterno

--- client/index.js
+++ client/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 class App extends Component {
   render () {
-    return <h1>Hello, world!</h1>
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <div>test</div>
+      </div>
+    )
   }
 }
 
